cd38dfc58f4525e918a6b614e8a409858d4bb1b0
Ahmad Mostafa <ahmad.essa94@eng-st.cu.edu.eg>
2018-08-12 19:17:16 +0200

    first commit

---
+++ Assets/GlobeTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobeTrigger : MonoBehaviour {
+
+    Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Globe_Trigger");
+        }
+	}
+}
